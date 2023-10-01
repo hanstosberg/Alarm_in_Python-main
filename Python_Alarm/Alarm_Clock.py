daf392973fddf054b888a246a9560c4b8a4ea2f3
Артём Голомышкин <hanstosberg@gmail.com>
2023-10-01 10:13:57 +0300

    button  add

--- Python_Alarm/Alarm_Clock.py
+++ Python_Alarm/Alarm_Clock.py
@@ -22,6 +22,8 @@ class AlarmClock:
         self.text_hour = Entry(self.root, bg='#CECECE', font='Cambria', justify='center', width=5)
         self.text_minutes = Entry(self.root, bg='#CECECE', font='Cambria', justify='center', width=5)
 
+        self.btn = Button(self.root, text=" Установить будильник ", width=35, height=2, bg='#6EA6C1',
+                          fg='#000000', font=('Verdana', 13, 'bold'))
         self.miusik = Label(self.root, text=f'Выберете мелодию:',
                             bg='#FFCC00', fg='#000000', bd=2, font='Verdana', width=20, height=2)
         self.variable = StringVar(self.root)
@@ -33,6 +35,7 @@ class AlarmClock:
         self.text_hour.place(x=260, y=100)
         self.text_minutes.place(x=260, y=140)
         self.miusik.place(x=10, y=190)
+        self.btn.place(x=0, y=270)
         self.shr.place(x=280, y=205)
         self.canvas.pack()
         self.root.mainloop()
