b200150211ed2890ce6dbfba0030eb81bb7ba758
Артём Голомышкин <hanstosberg@gmail.com>
2023-10-01 10:05:59 +0300

    music chose add

--- Python_Alarm/Alarm_Clock.py
+++ Python_Alarm/Alarm_Clock.py
@@ -17,11 +17,19 @@ class AlarmClock:
                              fg='#000000', bd=2, font='Verdana', width=15, height=1)
         self.text_hour = Entry(self.root, bg='#CECECE', font='Cambria', justify='center', width=5)
         self.text_minutes = Entry(self.root, bg='#CECECE', font='Cambria', justify='center', width=5)
+
+        self.miusik = Label(self.root, text=f'Выберете мелодию:',
+                            bg='#FFCC00', fg='#000000', bd=2, font='Verdana', width=20, height=2)
+        self.variable = StringVar(self.root)
+
+        self.shr = OptionMenu(self.root, self.variable, 'Мелодия 1', 'Мелодия 2', 'Мелодия 3', 'Мелодия 4', 'Мелодия 5')
+
         self.hour.place(x=45, y=100)
         self.minutes.place(x=45, y=140)
         self.text_hour.place(x=260, y=100)
         self.text_minutes.place(x=260, y=140)
-
+        self.miusik.place(x=10, y=190)
+        self.shr.place(x=280, y=205)
         self.canvas.pack()
         self.root.mainloop()
 
